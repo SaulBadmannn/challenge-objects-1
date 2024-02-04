f422df6387877e3ded1aedb5aa1a55003bdb25ed
Samat Bekkulov <66533036+SaulBadmannn@users.noreply.github.com>
2024-02-05 02:20:04 +0500

    Update index.test.js

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,27 +1,27 @@
 import test from 'node:test';
 import assert from 'assert/strict';
 
-import * as func from '../index.js'
+import dnaToRna from '../index.js'
 
 test('step1', () => {
-    const result = func.dnaToRna('ACGTGGTCTTAA');
+    const result = dnaToRna('ACGTGGTCTTAA');
     const expected = 'UGCACCAGAAUU';
     assert.strictEqual(result, expected);
 });
 
 test('step2', () => {
-    const result = func.dnaToRna('CCGTA');
+    const result = dnaToRna('CCGTA');
     const expected = 'GGCAU';
     assert.strictEqual(result, expected);
 });
 
 test('step3', () => {
-    const result = func.dnaToRna('');
+    const result = dnaToRna('');
     const expected = '';
     assert.strictEqual(result, expected);
 });
 
 test('step4', () => {
-    const result = func.dnaToRna('ACNTG');
+    const result = dnaToRna('ACNTG');
     assert.strictEqual(result, null);
 });
